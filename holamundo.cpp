1a55fc9339472dcb8211ad3f215728d0b0eb1a87
jperdomof <98925356+jperdomof@users.noreply.github.com>
2022-02-02 18:39:10 -0600

    Add files via upload

---
+++ holamundo.cpp
@@ -0,0 +1,18 @@
+/*
+  Proposito del programa: Mostrar el mensaje Hola mundo en pantalla
+   
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout<<"Hola Mundo"<<endl;
+
+ return 0;
+}
+
+
