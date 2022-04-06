a9ff8d0c694721d30746cd55fed9f36e327ac916
jperdomof <98925356+jperdomof@users.noreply.github.com>
2022-04-05 22:37:03 -0600

    Add files via upload

---
+++ Ejercicio 2-Mensajes/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	string nombre;
+	
+	cout<<"Ingrese el nombre: ";
+	cin>>nombre;
+	
+	cout<<endl;
+	
+	cout<<"Su nombre es: "<<nombre;
+	cout<<endl;
+		
+	return 0;
+}
