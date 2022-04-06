66af15c01debbf26e8cb5db1fc7050ba1af3970b
jperdomof <98925356+jperdomof@users.noreply.github.com>
2022-04-05 22:38:14 -0600

    Add files via upload

---
+++ Ejercicio 3-Variables.cpp
@@ -0,0 +1,24 @@
+/*
+  Proposito del programa: Variables 
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numeroentero;
+	numeroentero= 15;
+	
+	 cout<<"Entero: "<<numeroentero;
+	 
+	 numeroentero=18;
+	 
+	 cout<<" Entero modificado: "<<numeroentero;
+	 
+ return 0;
+}
+
+
---
+++ Ejercicio 4-Todas las variables.cpp
@@ -0,0 +1,29 @@
+/*
+  Proposito del programa: Todas las varibles
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	bool valorboolean;
+	valorboolean= true;
+	
+	int valorentero= 15;
+	double valordouble=20.99;
+	string valorstring= "Hola como estan";
+	char valorchar= 'B';
+	
+	cout<<"Valor boolean: "<<valorboolean<<endl;
+	cout<<"Valor Entero: "<<valorentero<<endl;
+	cout<<"Valor Double: "<<valordouble<<endl;
+	cout<<"Valor string "<<valorstring<<endl;
+	cout<<"Valor Char: "<<valorchar<<endl;
+	
+ return 0;
+}
+
+
