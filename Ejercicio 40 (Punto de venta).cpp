ecb7ef10d9f0ac2a594fefad274210953c3e325a
jperdomof <98925356+jperdomof@users.noreply.github.com>
2022-04-06 21:58:46 -0600

    Add files via upload

---
+++ Ejercicio 40 (Punto de venta).cpp
@@ -0,0 +1,39 @@
+/*
+  Proposito del programa: Menu de opciones punto de venta
+ 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+	
+
+{
+	int opcion= 0;
+	
+	while(true)
+	{
+		cout<<"****";
+		cout<<"MENU";
+		cout<<"****";
+		cout<<endl;
+		
+		cout<<"1 - Bebidas calientes"<<endl;
+		cout<<"2 - Bebidas Frias"<<endl;
+		cout<<"3 - Reposteria"<<endl;
+		cout<<"0 - Salir"<<endl;
+		
+		cin>>opcion; 
+	}
+	
+		
+
+
+ 
+ return 0;
+}
+
+
