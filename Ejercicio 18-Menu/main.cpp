8bc5bf81fe718f828753b2e5fdf04ef33f04a514
jperdomof <98925356+jperdomof@users.noreply.github.com>
2022-04-06 18:30:18 -0600

    Add files via upload

---
+++ Ejercicio 18-Menu/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int opcion= 0;
+	
+	while(true)
+	{
+		system("cls");
+		cout<<"*****"<<endl;
+		cout<<"MENU"<<endl;
+		cout<<"*****"<<endl;
+		
+		cout<<endl;
+		cout<<"1 - Cafe y granitas"<<endl;
+		cout<<"2 - Reposteria"<<endl;
+		cout<<"0 - Salir"<<endl;
+		
+		cout<<"Ingrese una opcion del menu:";
+		cin>>opcion;
+		
+		if(opcion== 1)
+		{
+			system("cls");
+			cout<<"\nEstas en el menu de reposteria";
+			system("pause");
+		}
+		
+		if(opcion== 2)
+		{
+			system("cls");
+			cout<<"\nEstas en el menu de reposteria";
+			system("pause");
+		}
+		if(opcion== 0 )
+		{
+			
+			break;
+						
+		}
+			cout<<endl;
+			cout<<"Saliste del sistema"<<endl;
+		
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
